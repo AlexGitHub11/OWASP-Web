97ed6d7cafd81e52f41ab783776dd1e52bc3f2fa
Alex GitHub <64652592+AlexGitHub11@users.noreply.github.com>
2024-02-10 15:54:21 +0000

    Updated WebApp files

---
+++ index.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <link rel="stylesheet" href=Style.css> 
+    </head>
+
+    <body text="white">
+
+    <nav class="navigation-bar">
+
+    <img src="images/logo.png" alt="navbarlogo" class="logo">
+
+    <a href ="index.php" class="button">Home</a>
+
+</nav>
+
+<section class="content">
+    <h1 class="title">OWASP Web</h1>
+
+<p>This Web Application has be formulated with the intent to test the user on their software security ability by exploiting four of the OWASP Top 10 vulnerabilities.<br><pr>
+
+<div class="container">
+    <a href="Lv1-Login.php"><button class="btn">Continue to Level 1</button></a>
+</div>
+
+
+<footer class="footer">
+    <br>Can you make it to level 5
+</footer>
+
+
+    </body>
+
+
+</html>
